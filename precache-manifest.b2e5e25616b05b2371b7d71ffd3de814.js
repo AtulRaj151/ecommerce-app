74583064039d07ce654935c4ceecfcad2074ca98
Atul Raj <atulsid151@gmail.com>
2020-08-24 02:49:12 +0530

    Updates

---
+++ precache-manifest.b2e5e25616b05b2371b7d71ffd3de814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374489563d99823d73019ca29f05cf52",
+    "url": "/ecommerce-app/index.html"
+  },
+  {
+    "revision": "ff4891aa36a17a05fc07",
+    "url": "/ecommerce-app/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5687ff72b92864276441",
+    "url": "/ecommerce-app/static/css/main.cc74196b.chunk.css"
+  },
+  {
+    "revision": "ff4891aa36a17a05fc07",
+    "url": "/ecommerce-app/static/js/2.b29a795b.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/ecommerce-app/static/js/2.b29a795b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5687ff72b92864276441",
+    "url": "/ecommerce-app/static/js/main.03429fbf.chunk.js"
+  },
+  {
+    "revision": "2887ab20419561b80ff6",
+    "url": "/ecommerce-app/static/js/runtime-main.9463bbce.js"
+  }
+]);
